4814e7984041b6a711553f70a45ee69351acf5d4
admin <peter@draxler.ml>
2022-03-24 15:21:11 +0100

    release

---
+++ scripts/updateReleaseNotes/createReleaseWithoutNotes.ts
@@ -0,0 +1,24 @@
+/**
+ * This script sends release notes to github. The release notes are pulled from
+ * CHANGELOG.json entries and are only sent if there aren't already notes for a
+ * given tag.
+ */
+
+import { Octokit } from '@octokit/rest';
+import { argv, repoDetails, github } from './init';
+import reactComponentsPackageJson from '../../packages/react-components/package.json';
+
+if (!argv.apply) {
+  console.error('\nERROR: createReleaseWithoutNotes does not support dry run. Use the "--apply" flag.\n');
+  process.exit(1);
+}
+
+const releaseDetails: Partial<Octokit.ReposUpdateReleaseParams> = {
+  ...repoDetails,
+  tag_name: reactComponentsPackageJson.version,
+  name: `react-components v${reactComponentsPackageJson}`,
+  draft: false,
+  prerelease: true,
+  // body: await getMarkdownForEntry(entry),
+};
+github.repos.createRelease(releaseDetails as Octokit.ReposCreateReleaseParams);
